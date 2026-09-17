4f413b72e42245ba267bcce4c696c9b3b69fc3bb
luto-esc <luciomatiasescalante1@gmail.com>
2026-09-17 20:05:49 -0300

    update

--- Capitulo1/Ejercicios/ejercicio1-22.c
+++ Capitulo1/Ejercicios/ejercicio1-22.c
@@ -6,25 +6,58 @@ tabuladores antes de la columna especifica */
 
 #include <stdio.h>
 
-#define LIM 10
-#define IN 1
-#define OUT 0
+#define LIMIN
+#define LIM 40
 
 
 int main(void)
 {
+  int LIMITE;
   int c;
-  int i;
-  int ind;
+  int i,j;
+  char line[4000];
   char sline[LIM];
-  char word[LIM];
+  int len; /* tamaño de la linea */
   int estado; /* bandera para ver si esta en una palabra */
   int ult_esp; /* lugar del ultimo espacio */
 
-  estado = IN;
-  i = 0;
-  while ((c = getchar()) != EOF && c != '\n'){
-    
+  /* cargamos la entrada en al arreglo general */
+  for (i=0; i < 4000 && (c = getchar()) != EOF && c != '\n';i++){
+    line[i] = c;
   }
-  
+
+  /* tomamos el tamaño de la linea de entrada */
+  len = 0;
+  while (line[len] != '\n'){
+    len++;
+  }
+
+  /* tratamos el arreglo */
+  i = 0;
+  j = 0;
+  LIMITE = 40;
+  while (i < len){
+    if (i == LIMITE || line[i] == '\n'){
+      if (line[i] == ' '){
+	printf("%s",sline);
+	putchar('\n');
+      }
+      else if (line[i] != ' '){
+	sline[ult_esp] = '\n';
+	printf("%s",sline);
+	j = 0;
+	sline[j] = line[i];
+      }
+      LIMITE = LIMITE * 2;
+    }
+    else{
+      if (c == ' '){
+	ult_esp = i;
+      }
+      sline[j] = line[i];
+    }
+    
+    i++;
+    j++;
+  }
 }
